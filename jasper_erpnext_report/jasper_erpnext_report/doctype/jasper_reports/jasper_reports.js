2d396f3d8d4b6e3dd23e9092ea6d229444ad1b4b
Luis Fernandes <luisfmfernandes@gmail.com>
2015-02-05 19:58:24 +0000

    Update javascript logic

--- jasper_erpnext_report/jasper_erpnext_report/doctype/jasper_reports/jasper_reports.js
+++ jasper_erpnext_report/jasper_erpnext_report/doctype/jasper_reports/jasper_reports.js
@@ -1,13 +1,15 @@
 
 cur_frm.cscript.refresh = function(doc){
-    
+    var cs = cur_frm.cscript;
     //$(cur_frm.get_field("query_html").wrapper).find('.query').text('luis')
     //this.set_value("query_html", "12234");
     //fortype can be "doctype" or "report"
-    var args = {fortype: "doctype", report_name:"Teste 1", doctype:"Jasper Reports", name_ids:["Administrator", "Guest"], pformat:"pdf"};
+    //var args = {fortype: "doctype", report_name:"Teste 1", doctype:"Jasper Reports", name_ids:["Administrator", "Guest"], pformat:"pdf"};
     //var df = jasper.run_jasper_report("run_report", args, null);
     
     //cur_frm.conn_test();
+    
+    cs.show_fields(doc);
     
     var query = doc.query != undefined? doc.query:""
     var code = '<div class="panel panel-default">'
@@ -24,9 +26,45 @@ cur_frm.cscript.refresh = function(doc){
     console.log("query: ", code)
 }
 
+cur_frm.cscript["jasper_report_origin"] = function(doc, dt, dn){
+    var origin = doc.jasper_report_origin;
+    if (origin === "JasperServer"){
+        hide_field(["jasper_upload_jrxml", "report_images:", "jasper_all_sites_report"]);
+        if (doc.__islocal){
+            cur_frm.set_value("jasper_report_path", "");
+        }
+        unhide_field(["jasper_report_path"]);
+    }else if(doc.__islocal){//never saved
+        hide_field(["jasper_upload_jrxml", "report_images:", "jasper_report_path"]);
+        cur_frm.set_value("jasper_report_path", "/");
+    }else{
+        unhide_field(["jasper_upload_jrxml", "report_images:", "jasper_all_sites_report"]);
+        hide_field(["jasper_report_path"]);
+    }
+}
+
+cur_frm.cscript.show_fields = function(doc){
+    var origin = doc.jasper_report_origin;
+    console.log("never saved ", doc.__islocal);
+    if (origin === "JasperServer"){
+        //cur_frm.set_df_property("jasper_upload_jrxml", "hidden", "True");
+        hide_field(["jasper_upload_jrxml", "report_images:", "jasper_all_sites_report"]);
+        //cur_frm.set_df_property("jasper_report_images", "hidden", "True");
+    }else if(doc.__islocal){//never saved
+        hide_field(["jasper_upload_jrxml", "report_images:", "jasper_report_path"]);
+        //doc.jasper_report_path = "/";
+        cur_frm.set_value("jasper_report_path", "/");
+        //cur_frm.refresh_field("jasper_report_path");
+    }else{
+        hide_field(["jasper_report_path"]);
+    }
+    
+};
+
 cur_frm.cscript.onload = function(doc){
     //$('.query').text("luis filipe")
     //cur_frm.fields_dict.query_html.$wrapper.html("<p>Luis</p>")
+    
 }
 
 //this is for testing liveupdates with threads, pubsub and queue
