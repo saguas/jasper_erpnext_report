1804d9929bd243fdc3d8a951e7e2aee2a7990a0e
Luis Fernandes <luisfmfernandes@gmail.com>
2015-04-16 11:29:31 +0100

    fix pyjnius java classpath

--- jasper_erpnext_report/jasper_reports/__init__.py
+++ jasper_erpnext_report/jasper_reports/__init__.py
@@ -1,18 +1,20 @@
 import os, logging, frappe
-import jasper_erpnext_report as jr
+#import jasper_erpnext_report as jr
+from jasper_erpnext_report import pyjnius
 
 _logger = logging.getLogger(frappe.__name__)
 
 try:
 	import jnius_config as jc
-	jr.pyjnius = True
+	pyjnius = True
 
 	if not jc.vm_running:
 		jc.add_options('-Djava.awt.headless=true')
 	else:
 		_logger.info("jasper_reports __init__ vm_running {}".format(jc.vm_running))
 except:
-	jr.pyjnius = False
+	print "jnius_config not found"
+	pyjnius = False
 
 norm_path = os.path.normpath
 join_path = os.path.join
@@ -21,9 +23,9 @@ parent_path = dirname(dirname(__file__))
 rel_path = os.path.relpath(os.path.join(parent_path, "java"),dirname(__file__))
 rel_path_curr = os.path.relpath(parent_path, os.getcwd())
 try:
-    os.environ['CLASSPATH'] = os.environ['CLASSPATH'] + ":" +norm_path(join_path(parent_path,"java/lib/*")) + ":."
+	os.environ['CLASSPATH'] = os.environ.get('CLASSPATH',"") + ":" +norm_path(join_path(parent_path,"java/lib/*")) + ":."
 except:
-    pass
+	print "Error in setting java classpath."
 
 try:
 	from jnius import autoclass
@@ -39,7 +41,8 @@ try:
 	ReportCompiler = getJavaClass('ReportCompiler')
 
 	ExportReport = getJavaClass('ExportReport')
-	jr.pyjnius = True
-	print "pyjnius is ok 4: {}".format(jr.pyjnius)
-except:
-	jr.pyjnius = False
+	pyjnius = True
+	print "pyjnius is ok 4: {}".format(pyjnius)
+except Exception, e:
+	print "pyjnius is not ok {}".format(e)
+	pyjnius = False
--- jasper_erpnext_report/jasper_reports/compile_reports.py
+++ jasper_erpnext_report/jasper_reports/compile_reports.py
@@ -7,7 +7,11 @@ _logger = logging.getLogger(frappe.__name__)
 def jasper_compile(jrxml, destFileName):
 	_logger.info("jasper_compile jrxml dir {0} destFileName {1}".format(jrxml, destFileName))
 	try:
+		print "pyjnius {}".format(jr.pyjnius)
 		compiler = jr.ReportCompiler()
 		compiler.compile(jrxml,destFileName)
 	except Exception as e:
+		if jr.pyjnius == False:
+			frappe.throw(_("Please install pyjnius python module."))
+			return
 		frappe.throw(_("Error while compiling report %s, error is: %s." % (jrxml, e)))
